fc1f600bc568c957fb6451f11104185cf4495592
Andy Pan <andy0130tw@yahoo.com.tw>
2015-04-21 23:31:32 +0800

    handler, formatters, etc.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var math = require('mathjs');
 var bodyParser = require('body-parser');
+
+var handlers = require('./lib/handlers');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 8080));
@@ -13,78 +16,5 @@ app.listen(app.get('port'), function () {
 
 var mathParser = new math.parser();
 
-app.get('/eval', function(req, resp) {
-	resp.send('/eval only currently accept POST requests.');
-});
-
-app.post('/eval', function(req, resp) {
-  var result = {};
-  var post = req.body;
-  var expr = post.text;
-  var trigger = post.trigger_word;
-
-  // do not respond to bot-users
-  if(post.user_name == 'slackbot'){
-  	resp.end();
-  	return;
-  }
-
-  if (!expr) {
-  	result.ok = false;
-  	result.text = 'No message sent.';
-    resp.json(result);
-    return;
-  }
-
-  if (trigger) {
-  	expr = expr.slice(trigger.length);
-  }
-  
-  result.text = '\00';
-  
-  var dispName = '@' + post.user_name;
-
-  try {
-    result.ok = true;
-    var answer = mathParser.eval(expr);
-    if (!(answer instanceof Function)) {
-	    result.attachments = [{
-	    	color: 'good',
-	    	fallback: dispName + ': ans = ' + answer,
-	    	text: dispName + ': ans = *' + answer + '*',
-	    	mrkdwn_in: ['text']
-	    }];
-    } else {
-    	var funcName = getFuncName(answer);
-    	result.attachments = [{
-    		color: 'warning',
-    		fallback: dispName + ': { Function ' + funcName + ' }',
-    		text: dispName + ': { Function `' + (funcName ? ('`' + funcName + '`') : '') + '` }',
-    		mrkdwn_in: ['text']
-    	}];
-    }
-    mathParser.scope.ans = answer;
-  } catch (err) {
-    result.ok = false;
-    result.attachments = [{
-    	color: 'danger',
-    	fallback: dispName + ' ' + err.toString(),
-    	text: dispName + ' *' + err.name + '*: ' + err.message,
-    	mrkdwn_in: ['text']
-    }];
-    result.error = {
-      type: err.name,
-      message: err.message
-    };
-  }
-
-  resp.json(result);
-
-});
-
-function getFuncName(f) {
-	// http://stackoverflow.com/a/17923727/2281355
-	var matches = /^function\s+([\w\$]+)\s*\(/.exec( f.toString() );
-	return matches ? matches[1] : null;
-}
-
+app.get('/eval', handlers.evalGet);
+app.post('/eval', handlers.evalPost);
