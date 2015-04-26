409d9e09bba4900f292c69d0828797914960fe3c
Andy Pan <andy0130tw@yahoo.com.tw>
2015-04-27 00:22:11 +0800

    add ping itself
    ping itself every 20 minutes to keep it awake on Heroku.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var request = require('request');
 var math = require('mathjs');
 var bodyParser = require('body-parser');
 
@@ -12,6 +13,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(app.get('port'), function () {
   console.log('app starting at port '+ app.get('port'));
+  
+  var appName = process.env.APP_NAME;
+  if(appName)
+    setInterval(function () {
+      console.log('ping every 20 minutes');
+  	  request.get('http://' + appName + '.herokuapp.com/ping');
+    }, 20 * 60 * 1000);
+
 });
 
 var mathParser = new math.parser();
@@ -23,3 +32,6 @@ app.post('/eval', handlers.evalPost);
 
 app.get('/scope/read', handlers.scopeReadGet);
 
+app.get('/ping', function(req, resp) {
+  resp.send('pong');
+});
